159a5e4a0004164516a2a120fa93d7e6a9d6ca1b
iskrenr <iskrenr005@gmail.com>
2023-09-28 14:55:01 +0200

    Created 3 instances of User

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.library.User;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user1 = new User("Aljoscha", "Nyang");
+        User user2 = new User("Fabian", "Link");
+        User user3 = new User("Iskren", "Radev");
+
     }
 }
