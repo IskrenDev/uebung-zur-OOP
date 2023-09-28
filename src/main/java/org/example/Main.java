67f2fa79d23b5d051c275f4b217a1e0a2e474f76
iskrenr <iskrenr005@gmail.com>
2023-09-28 15:12:25 +0200

    Created 5 instances of Book

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.library.User;
+import org.example.library.Book;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,12 @@ public class Main {
         User user3 = new User("Iskren", "Radev");
 
         System.out.println(user1);
+
+        Book book1 = new Book("Der Steppenwolf", "Hermann Hesse", "978-3-596-90078-9");
+        Book book2 = new Book("Die Verwandlung", "Franz Kafka", "978-3-596-90400-8");
+        Book book3 = new Book("Im Westen nichts Neues", "Erich Maria Remarque", "978-3-499-22755-5");
+        Book book4 = new Book("Der Vorleser", "Bernhard Schlink", "978-3-442-31056-7");
+        Book book5 = new Book("Das Parfum", "Patrick Süskind", "978-3-257-23028-4");
 
     }
 }
